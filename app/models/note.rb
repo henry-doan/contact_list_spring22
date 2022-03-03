6cb735e898095792bef306f2069aa81a891b7a42
Henry Doan <henrydoan96@gmail.com>
2022-03-02 20:47:31 -0700

    end of models

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,7 @@
 class Note < ApplicationRecord
   belongs_to :contact
   has_many :comments, dependent: :destroy
+
+  validates :body, presence: true 
+  validates :body, length: { in: 10...500 }
 end
